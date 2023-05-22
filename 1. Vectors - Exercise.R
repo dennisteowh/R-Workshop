adcf7a27a9e2aad396f7db5444dd69ff3a08a029
dennisteowh <59493837+dennisteowh@users.noreply.github.com>
2023-05-22 10:47:35 +0800

    Add files via upload

--- 1. Vectors - Exercise.R
+++ 1. Vectors - Exercise.R
@@ -7,6 +7,7 @@
 
 animals <- c("Mufasa", "Simba", "Timon", "Pumbaa", "Nala")
 weight <- c(40, 20, 3, 1000, 19)
+species <- c("lion", "lion", "meerket", "warthog", "lion")
 
 # 1) Which animals weigh less than 20kg?
 
@@ -17,17 +18,24 @@ weight <- c(40, 20, 3, 1000, 19)
 # 3) Does Mufasa weigh less than 20kg?
 
 
-# 4) Sort the animals in your vectors in alphabetical order
+# 4) Which lions weigh more than or equal to 20kg?
 
 
-# 5) What is the last animal (ordered alphabetically)?
+# 5) Sort the animals in alphabetical order (just print the answer out, no need to modify the original animals vector)
 
 
-# 6) Does the last animal (ordered alphabetically) weigh exactly 3 kg?
+# 6) Sort the animals according to their weight (in descending order; just print the answer out, no need to modify the original animals vector)
 
 
-# 7) Add another animal called "Zazu" to the animals vector and 
+# 7) What is the last animal (ordered alphabetically)?
+
+
+# 8) Does the last animal (ordered alphabetically) weigh exactly 3 kg?
+
+
+# 9) Add another animal called "Zazu" to the animals vector and 
 #    add another weight of 1.2 (corresponding to Zazu's weight) to the weight vector 
+#    add another species of "hornbill" (corresponding to Zazu's species) to the species vector
 
 
-# 8) Rerun your code from the previous questions. Do they still give the correct answers?
+# 10) Rerun your code from the previous questions. Do they still give the correct answers?
